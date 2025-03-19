4e1be7b44fb25a9828115e0c7883064a5aff576f
bempoj <bence29q@gmail.com>
2025-03-19 18:58:38 +0100

    json test case schema upload

--- pdf_extractor.py
+++ pdf_extractor.py
@@ -1,11 +1,32 @@
 import pymupdf
 import os
+import json
 from tkinter import Tk, filedialog
-from config import *
+#from config import *
 from openai import OpenAI
 
 chunk_size = 3
 client = OpenAI(api_key = "")
+
+def load_json():
+    Tk().withdraw()
+    json_path = filedialog.askopenfilename(
+        title="Select a Test Case Schema JSON file",
+        filetypes=[("JSON files", "*.json")]
+    )
+
+    if not json_path:
+        print("No JSON file selected!")
+        return {}
+
+    try:
+        with open(json_path, "r", encoding="utf-8") as schema_file:
+            schema_data = json.load(schema_file)
+            print("\nSchema loaded successfully!")
+            return schema_data.get("schema", {})
+    except Exception as e:
+        print(f"Error loading file: {e}")
+        return {}
 
 def pdf_reader(pdf_path):
     doc = pymupdf.open(pdf_path)
@@ -35,6 +56,11 @@ pdf_path = filedialog.askopenfilename(
 if pdf_path:
     extracted_text = pdf_reader(pdf_path)
     
+    test_case_schema = load_json()
+    
+    if not test_case_schema:
+        print("Schema failed to load.")
+    
     temp_chunks = 0
     current_index = 0
     current_chunk = ''
@@ -44,9 +70,15 @@ if pdf_path:
         
         if temp_chunks == chunk_size:
             print("\n Sending PDF content to OpenAI...")
+            
             #print(current_chunk)
-            response = send_to_openai(f"Here’s the current text chunk from my PDF:\n\n{current_chunk}\n\nCan you write test cases based on this content?")
+            
+            response = send_to_openai(
+                f"Here’s the current text chunk from my PDF:\n\n{current_chunk}"
+                f"\n\nCan you write test cases based on this content, following this schema:\n{json.dumps(test_case_schema, indent=2)}"
+                )
             print(response)
+            
             current_chunk = ''
             temp_chunks = 0
             
@@ -54,5 +86,5 @@ if pdf_path:
         current_index += 1
     
 else:
-    print("No file selected!")
+    print("No PDF file selected!")
 
